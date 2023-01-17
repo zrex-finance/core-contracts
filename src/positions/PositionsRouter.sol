cff17dd92cd181007338ca6f2e0882422c4dec79
nikdementev <puffingcheeks@gmail.com>
2023-01-18 01:24:55 +0300

    :sparkles: open & close position with executor

--- src/positions/PositionsRouter.sol
+++ src/positions/PositionsRouter.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+interface IExecutor {
+    function execute(
+        string[] calldata _targetNames,
+        bytes[] calldata _datas,
+        address _origin
+    ) external payable;
+}
+
 
 contract PositionsRouter {
     struct Position {
@@ -11,8 +19,19 @@ contract PositionsRouter {
         uint256 sizeDelta;
     }
 
+    IExecutor private immutable executor;
+
     mapping (bytes32 => Position) public positions;
     mapping (address => uint256) public positionsIndex;
+
+    modifier onlyCallback() {
+        require(msg.sender == address(this), "Access denied");
+        _;
+    }
+
+    constructor(IExecutor _executor) {
+        executor = _executor;
+    }
 
     function openPosition(Position memory position) external payable {
         require(position.account == msg.sender, "Only owner");
@@ -26,7 +45,34 @@ contract PositionsRouter {
         positions[key] = position;
     }
 
+    function closePosition(bytes32 key) external payable {
+        Position memory position = positions[key];
+
+        require(msg.sender == position.account, "Can close own position or position available for liquidation");
+
+        delete positions[key];
+    }
+
     function getKey(address _account, uint256 _index) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(_account, _index));
+    }
+
+    function openPositionCallback(
+        string[] calldata _targetNames,
+        bytes[] calldata _datas,
+        address _origin,
+        uint256 /* repayAmount */
+    ) external payable onlyCallback {
+        executor.execute(_targetNames, _datas, _origin);
+    }
+
+    function closePositionCallback(
+        string[] calldata _targetNames,
+        bytes[] calldata _datas,
+        address _origin,
+        uint256 /* repayAmount */
+    ) external payable onlyCallback {
+        executor.execute(_targetNames, _datas, _origin);
+
     }
 }
