ebd9f6a59191927f973eec68707c420c363f2cff
nikdementev <puffingcheeks@gmail.com>
2023-03-01 21:32:10 +0300

    :white_check_mark: rename import

--- test/position-compound.t.sol
+++ test/position-compound.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "../src/positions/Router.sol";
+import "../src/positions/router.sol";
 import "../src/positions/interfaces.sol";
 
 import "../src/connectors/protocols/compound/v3/main.sol";
