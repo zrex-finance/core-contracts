371334b857137b5a9773244bbe9c4e9a71321fc0
nikdementev <puffingcheeks@gmail.com>
2023-04-04 20:03:20 +0300

    :page_facing_up: add MIT license

--- contracts/interfaces/ICompoundV2Connector.sol
+++ contracts/interfaces/ICompoundV2Connector.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import { CErc20Interface } from './external/compound-v2/CTokenInterfaces.sol';
--- contracts/mocks/EthConverter.sol
+++ contracts/mocks/EthConverter.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import { IERC20 } from '../dependencies/openzeppelin/contracts/IERC20.sol';
