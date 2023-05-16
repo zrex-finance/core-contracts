f3f193dc1368e1ebc706ed5b6ba75b0ff30828f5
nikdementev <puffingcheeks@gmail.com>
2023-05-16 12:30:03 +0300

    :mute: Remove account logs

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -17,15 +17,13 @@ import { IConnectors } from './interfaces/IConnectors.sol';
 import { IBaseFlashloan } from './interfaces/IBaseFlashloan.sol';
 import { IAddressesProvider } from './interfaces/IAddressesProvider.sol';
 
-import 'forge-std/Test.sol';
-
 /**
  * @title Account
  * @author FlashFlow
  * @notice Contract used as implimentation user account.
  * @dev Interaction with contracts is carried out by means of calling the proxy contract.
  */
-contract Account is Initializable, IAccount, Test {
+contract Account is Initializable, IAccount {
     using UniversalERC20 for IERC20;
     using ConnectorsCall for IAddressesProvider;
     using Address for address;
