553fde0efc7fc145b2e309c20bd016cf74b29e79
nikdementev <puffingcheeks@gmail.com>
2023-05-31 13:27:27 +0300

    :twisted_rightwards_arrows: Merge with master

--- contracts/interfaces/IACLManager.sol
+++ contracts/interfaces/IACLManager.sol
@@ -2,29 +2,29 @@
 pragma solidity ^0.8.17;
 
 interface IACLManager {
-    function setRoleAdmin(bytes32 role, bytes32 adminRole) external;
+    function setRoleAdmin(bytes32 _role, bytes32 _adminRole) external;
 
-    function addConnectorAdmin(address admin) external;
+    function addConnectorAdmin(address _admin) external;
 
-    function removeConnectorAdmin(address admin) external;
+    function removeConnectorAdmin(address _admin) external;
 
-    function addRouterAdmin(address admin) external;
+    function addRouterAdmin(address _admin) external;
 
-    function removeRouterAdmin(address admin) external;
+    function removeRouterAdmin(address _admin) external;
 
-    function addReferralAdmin(address admin) external;
+    function addReferralAdmin(address _admin) external;
 
-    function removeReferralAdmin(address admin) external;
+    function removeReferralAdmin(address _admin) external;
 
-    function isConnectorAdmin(address admin) external view returns (bool);
+    function isConnectorAdmin(address _admin) external view returns (bool);
 
-    function isRouterAdmin(address admin) external view returns (bool);
+    function isRouterAdmin(address _admin) external view returns (bool);
 
-    function isReferralAdmin(address admin) external view returns (bool);
+    function isReferralAdmin(address _admin) external view returns (bool);
 
-    function addAssetListingAdmin(address admin) external;
+    function addAssetListingAdmin(address _admin) external;
 
-    function removeAssetListingAdmin(address admin) external;
+    function removeAssetListingAdmin(address _admin) external;
 
-    function isAssetListingAdmin(address admin) external view returns (bool);
+    function isAssetListingAdmin(address __admi) external view returns (bool);
 }
